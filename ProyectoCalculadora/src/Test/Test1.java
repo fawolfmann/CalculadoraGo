dfa01e148bb56b9c41f36e1e678f09420790220e
fabian <fawolfmann@gmail.com>
2016-04-10 21:14:43 -0300

    agrego gradle y travis

---
+++ ProyectoCalculadora/src/Test/Test1.java
@@ -0,0 +1,14 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1 {
+
+	@Test
+	public final void testCalculadora() {
+		fail("Not yet implemented");
+	}
+
+}
